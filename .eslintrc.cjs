bf9daa59cf5144982d43574fd03db0ed6de6f90e
J-Meira <jonathanmeira@hotmail.com>
2024-05-08 16:05:53 -0300

    change to pnpm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,13 +9,18 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'storybook-static', 'stories', 'clean.ts', 'copy.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'import'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'import/newline-after-import': ['error', { count: 1 }],
     "@typescript-eslint/no-explicit-any": "off",
+    // "react-hooks/rules-of-hooks": "error",
+    // "react-hooks/exhaustive-deps": "warn",
+    // "react/jsx-uses-react": "off",
+    // "react/react-in-jsx-scope": "off",
+    // "@typescript-eslint/no-non-null-assertion": "off",
+    // "@typescript-eslint/ban-ts-comment": "off",
   },
 }
