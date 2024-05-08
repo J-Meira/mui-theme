7181f11d0aab6ab0b64ed88e8edc35a1b74f9623
J-Meira <jonathanmeira@hotmail.com>
2024-05-08 12:03:46 -0300

    eslint fixed

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'storybook-static', 'stories', 'clean.ts', 'copy.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'import/newline-after-import': ['error', { count: 1 }],
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+}
